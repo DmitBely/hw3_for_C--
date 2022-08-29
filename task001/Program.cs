6914f4fce5990b8a83d8defb9f11d81f1ab6db6f
dmitriy <dmitbely@protonmail.ch>
2022-08-29 19:38:36 +0300

    tak001

---
+++ task001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Найти кубы чисел от 1 до N
+
+void FillArray(int[] col)
+{   
+    int i = 0;
+    int sum = 0;
+    while (i < col.Length)
+    {
+        sum = sum + 1;
+        col[i] = sum;
+        i++;
+    }
+}
+
+void Cube(int[] col)
+{
+    for (int i = 0; i < col.Length; i++) col[i] = col[i] * col[i] * col[i];
+
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++) Console.Write($"{col[i]} ");
+    {
+        
+    }
+}
+
+Console.WriteLine("Введите число до которого нужно сосчитать кубы: ");
+int limit = int.Parse(Console.ReadLine());
+int[] arr = new int[limit];
+
+FillArray(arr);
+Cube(arr);
+PrintArray(arr);
