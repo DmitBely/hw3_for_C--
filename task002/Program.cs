f7ae5f2a674776b01bd65c7c32f361e6a3497ade
dmitriy <dmitbely@protonmail.ch>
2022-08-29 20:20:33 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("В какую степень его нужно возвести?: ");
+int B = int.Parse(Console.ReadLine());
+
+
+void Magic(int number, int grade)
+{   
+    int res = number;
+    for (int i = 0; i < grade; i++) res *= grade;
+    Console.Write(res);
+
+}
+
+Magic(A, B);
