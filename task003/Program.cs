28c5912c82a95c6add47f159aea6423753ef4a62
dmitriy <dmitbely@protonmail.ch>
2022-08-30 09:31:20 +0300

    task003

---
+++ task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+void SumDigit (int num)
+{   
+    int sum = 0;
+    while (num % 10 > 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    Console.WriteLine($"Сумма цифр числа = {sum} ");
+}
+
+SumDigit(number);
