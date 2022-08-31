70d1a0bbdaebe396954feb5cf3988d7380a618df
dmitriy <dmitbely@protonmail.ch>
2022-08-31 12:01:58 +0300

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+
+int[] NewArray(int num)
+{
+    int[] arr = new int[num];
+    int i = 0;
+
+    while (i < arr.Length) 
+    {
+        arr[i] = new Random().Next(0,2); 
+        i++;
+    }
+    return arr;
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++) Console.Write($"{col[i]} ");
+    Console.WriteLine(" ");
+}
+
+int number = 8;
+int[] arr = NewArray(number);
+PrintArray(arr); 
